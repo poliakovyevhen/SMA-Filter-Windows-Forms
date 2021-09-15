73e2494e7244d386e180c4365ab085250bc1878f
Yevhen Poliakov <90283399+poliakovyevhen@users.noreply.github.com>
2021-09-15 21:17:44 +0300

    SMA Filter
    SMA Filter

---
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(268, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 119);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "УКР";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(69, 119);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "EN";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(300, 129);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(43, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Автори";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 151);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Фільтр з середнім ковзаючим";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*int N;
+            string[] Q = new string[90];
+            string[] F = new string[90];
+            for (int i = 0; i < Q.Length; i++)
+            {
+                Q[i] = textBox1.Text;
+
+
+                if (Q[i] == ",")
+                {
+                    F[i] = Q[i - 1];
+
+                }
+            }
+                for (int i = 0; i < F.Length; i++)
+                {
+                    textBox2.Text = F[i];
+                }
+                */
+            int a = 0;
+            int sum = 0;
+            double sma;
+            var Digits = textBox1.Text.Split(' ');
+            int[] Data = new int[Digits.Length];
+            for(int i=0; i<Digits.Length; i++)
+            {
+
+                sum = int.Parse(Digits[i]) + sum;
+                a++;
+            }
+            sma = sum / a;
+
+            /*char[] Q = new char[10];
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                Q[i] = textBox1.Text[i];
+
+            }
+
+            string z;
+            double sum=0;
+            double y;
+            string sma;
+            for (int i = 0; i <= 9; i++)
+            {
+                
+                sum=Q[i]+sum;
+            }
+            /*double x1 = Convert.ToDouble(Q[i]);
+            double x2 = Convert.ToDouble(textBox2.Text);
+            double x3 = Convert.ToDouble(textBox3.Text);
+            double x4 = Convert.ToDouble(textBox4.Text);
+            sum = x1 + x2 + x3 + x4;*/
+            string y;
+            y = Convert.ToString(sma);
+            textBox2.Text = y;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Text = "Фільтр з середнім ковзаючим";
+            button1.Text = "Пуск";
+            linkLabel1.Text = "Автори";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Text = "Simple moving avarage filter";
+            button1.Text = "Start";
+            linkLabel1.Text = "Autors";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Студенти:" + "\r\n" +
+                "Мільке Денис, Поляков Євген, Мелешко Сергій" + "\r\n" +
+                "Група СЕ-307а" + "\r\n" +
+                "Кафедра електроніки" + "\r\n" +
+                "ННІАЕТ" + "\r\n" +
+                "НАУ" + "\r\n" +
+                "Україна, Київ",
+                "Реквізити авторів:",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+                );
+        }
+    }
+}
+
